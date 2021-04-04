079e93e999c7b949bdbcaab825a60c658a623716
Artem Safin <artemsafin.webdev@gmail.com>
2021-04-05 01:13:52 +0300

    Уведомление об успешной отправке формы

--- src/assets/js/backend.js
+++ src/assets/js/backend.js
@@ -1,3 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
-})
+    var contactForm = document.querySelector('#contact-form');
+
+    if (contactForm) {
+        var expertForm = document.querySelector('.expert-form');
+        contactForm.addEventListener('submit', function(event) {
+            event.preventDefault();
+            if (
+                $(contactForm)
+                    .parsley()
+                    .isValid()
+            ) {
+                contactForm.reset();
+                $(contactForm)
+                    .parsley()
+                    .reset();
+                expertForm.classList.add('success');
+                setTimeout(function() {
+                    expertForm.classList.remove('success');
+                }, 2000);
+            }
+        });
+    }
+});
