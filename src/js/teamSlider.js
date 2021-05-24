5d76f985d21a0a9e7e6e81dd6c01714507c70d7c
Artem Safin <artemsafin.webdev@gmail.com>
2021-05-25 01:55:00 +0300

    Фикс по слайдеру команды

--- src/js/teamSlider.js
+++ src/js/teamSlider.js
@@ -1,9 +1,15 @@
-import CardSlider from "./mwSlider";
+import CardSlider from './mwSlider';
 
 export default function teamSlider() {
     const elements = Array.from(document.querySelectorAll('.js-team-slider'));
 
     elements.forEach(element => {
+        const cards = Array.from(element.querySelectorAll('.team__card'));
+        const controls = element.querySelector('.team__slider-controls')
+        if (window.matchMedia('(min-width: 641px)').matches && cards.length < 4) {
+            controls.style.display = 'none';
+            return;
+        }
         new CardSlider(element);
-    })
+    });
 }
